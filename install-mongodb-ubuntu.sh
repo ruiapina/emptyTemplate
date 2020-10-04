982a5ec3ff7a312c1813d242846dd90bf7ff1cb6
Rui Pina <8642418+ruiapina@users.noreply.github.com>
2020-10-04 11:56:51 +0200

    Update install-mongodb-ubuntu.sh

--- install-mongodb-ubuntu.sh
+++ install-mongodb-ubuntu.sh
@@ -1,17 +1,12 @@
 
-#https://askubuntu.com/questions/842592/apt-get-fails-on-16-04-or-18-04-installing-mongodb
-sudo apt remove --autoremove mongodb-org
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 
-sudo rm /etc/apt/sources.list.d/mongodb*.list
-sudo apt update
-
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 4B7C549A058F8B6B
-
-echo "deb [arch=amd64] http://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 
 sudo apt update
-sudo apt install mongodb-org
 
-sudo systemctl unmask mongod.service
-sudo systemctl enable mongod.service
-sudo systemctl start mongod.service
+sudo apt install -y mongodb-org
+
+sudo systemctl start mongod
+sudo systemctl enable mongod
+sudo systemctl status mongod
