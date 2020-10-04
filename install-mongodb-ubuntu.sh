4845bc3592bbacfadd5defda74291df3006e69af
Rui Pina <8642418+ruiapina@users.noreply.github.com>
2020-10-04 11:34:15 +0200

    Update install-mongodb-ubuntu.sh

--- install-mongodb-ubuntu.sh
+++ install-mongodb-ubuntu.sh
@@ -1,3 +1,7 @@
+wget -SO /dev/null http://in.archive.ubuntu.com/ubuntu/dists/xenial/InRelease
+
+wget -SO /dev/null http://archive.ubuntu.com/ubuntu/dists/xenial/InRelease
+
 # Configure mongodb.list file with the correct location
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
